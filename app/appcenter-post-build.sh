a0ff13d8945c1820cf8a4c159e0120d6bba52e01
lz233 <lz23333@outlook.com>
2020-04-17 14:34:17 +0800

    233

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -2,6 +2,7 @@ if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
 	wget https://github.com/iikira/BaiduPCS-Go/releases/download/v3.6.2/BaiduPCS-Go-v3.6.2-linux-amd64.zip
 	unzip BaiduPCS-Go-v3.6.2-linux-amd64.zip
 	cd BaiduPCS-Go-v3.6.2-linux-amd64
+	chmod 777 ./BaiduPCS-Go
 	./BaiduPCS-Go login -bduss=${BDUSS}
 	./BaiduPCS-Go config set -savedir=./ -user_agent="Mozilla;5.0" -max_parallel=500 -cache_size=256KB
 	./BaiduPCS-Go d "$FILE_DIR"
