aba2aeeeed8a9264dd53da1c97a38c4d72f1971b
lz233 <lz23333@outlook.com>
2020-04-17 16:16:31 +0800

    233

--- app/post.sh
+++ app/post.sh
@@ -3,7 +3,7 @@ unzip BaiduPCS-Go-v3.6.2-linux-386.zip
 cd BaiduPCS-Go-v3.6.2-linux-386
 chmod 777 ./BaiduPCS-Go
 ./BaiduPCS-Go login -bduss=${BDUSS}
-./BaiduPCS-Go config set -savedir=./ -user_agent="Mozilla;5.0" -max_parallel=500 -cache_size=256KB
+./BaiduPCS-Go config set -savedir=./ -user_agent="Mozilla;5.0" -max_parallel=2048 -cache_size=256KB
 ./BaiduPCS-Go cd "/$FILE_DIR"
 ./BaiduPCS-Go d "$FILE_NAME"
 cd ./$(find ./ -name '*_*' -type d)
