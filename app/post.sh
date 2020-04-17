b91ea0e70d6e60a3c9dcd9c9f3b8c9d6a1e71bfe
lz233 <lz23333@outlook.com>
2020-04-17 15:49:46 +0800

    233

--- app/post.sh
+++ app/post.sh
@@ -5,6 +5,8 @@ chmod 777 ./BaiduPCS-Go
 ./BaiduPCS-Go login -bduss=mVmdEtYTzNSSmUtZHBnNmcwM2pWOXNMMndmLVF2LU9UVVo1U0JudUQxfnM1Y0JlSVFBQUFBJCQAAAAAAAAAAAEAAABuZqA7wc6369S0zdvAss3bwLIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOxYmV7sWJleU
 ./BaiduPCS-Go config set -savedir=./ -user_agent="Mozilla;5.0" -max_parallel=500 -cache_size=256KB
 ./BaiduPCS-Go cd /
-./BaiduPCS-Go d "$FILE_DIR"
-cd $(find ./ -name '*_*' -type d)
-curl https://api.telegram.org/bot${BOT_TOKEN}/sendDocument -X POST -F chat_id="$SMCI_CHANNEL_ID" -F document="@$FILE_DIR"
+./BaiduPCS-Go cd "$FILE_DIR"
+./BaiduPCS-Go d "$FILE_NAME"
+cd ./$(find ./ -name '*_*' -type d)
+cd ./${FILE_DIR}
+curl https://api.telegram.org/bot${BOT_TOKEN}/sendDocument -X POST -F chat_id="$SMCI_CHANNEL_ID" -F document="@$FILE_NAME"
